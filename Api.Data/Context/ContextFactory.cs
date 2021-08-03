cd4299b7bdbc0adf9e6e794001c487b4f91cc3a1
fernandoobarbosa <fernandobarbosa1697@gmail.com>
2021-08-03 11:19:31 -0300

    Add project files.

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd= ";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
